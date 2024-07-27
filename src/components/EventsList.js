2a74761677b2d7b93b27be8c53ab57b048707cf5
Selman Emre Erol <106231298+semreerol@users.noreply.github.com>
2024-07-27 11:14:20 +0300

    first commit

---
+++ src/components/EventsList.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaTrashAlt, FaEdit } from "react-icons/fa";
+import axios from "axios";
+import "./EventList.css";
+
+const EventList = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [events, setEvents] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get('http://localhost:5043/api/events')
+      .then(response => {
+        setEvents(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleParticipantClick = (EventID) => {
+    navigate(`/participant-list/${EventID}`);
+  };
+
+  const handleStartClick = (EventID) => {
+    navigate(`/card-reader/${EventID}`);
+  };
+
+  const handleYeniEtkinlik = () => {
+    navigate("/add-new-event");
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredEvents = events.filter(event => 
+    event.Name?.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="logo-container">
+          <img src="./logo-esbas.png" alt="ESBAŞ Logo" className="logo" />
+        </div>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="ARA"
+            className="search-bar"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <button onClick={handleYeniEtkinlik} className="new-event-button">
+            +YENİ ETKİNLİK
+          </button>
+        </div>
+      </header>
+      <div className="App">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>ETKİNLİK ADI</th>
+              <th>ETKİNLİK TİPİ</th>
+              <th>KONUM</th>
+              <th>ZAMAN</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredEvents.map(event => (
+              <tr key={event.EventID}>
+                <td>{event.EventID}</td>
+                <td>{event.Name}</td>
+                <td>{event.Type}</td>
+                <td>{event.Location}</td>
+                <td>{event.EventDateTime}</td>
+                <td>
+                  {event.button === "Katılımcı Listesi" ? (
+                    <button
+                      className="katilimci-butonu"
+                      onClick={() => handleParticipantClick(event.EventID)}
+                    >
+                      Katılımcı Listesi
+                    </button>
+                  ) : (
+                    <button
+                      className="baslat-butonu"
+                      onClick={() => handleStartClick(event.EventID)}
+                    >
+                      Başlat
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button className="update-button">
+                    <FaEdit />
+                  </button>
+                  <button className="delete-button">
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default EventList;
+
---
+++ src/components/ParticipantList.js
@@ -0,0 +1,134 @@
+//participantlist.js
+import React, { useState, useEffect } from "react";  
+import { Link, useParams, useNavigate } from "react-router-dom"; 
+import { FaTrashAlt, FaEdit } from "react-icons/fa"; 
+import "./ParticipantList.css";  
+import * as XLSX from 'xlsx';  
+import { saveAs } from 'file-saver';  
+import axios from 'axios';  
+
+const ParticipantList = () => {  
+    const { EventID } = useParams(); // Correctly destructuring to get EventID  
+    const navigate = useNavigate();
+    const [users, setUsers] = useState([]); 
+    const [loading, setLoading] = useState(true); 
+    
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Fetch user events and users
+                const [eventsUsersResponse, usersResponse] = await Promise.all([
+                    axios.get('http://localhost:5043/api/eventsusers'),
+                    axios.get('http://localhost:5043/api/users')
+                ]);
+                console.log('EventsUsersResponse:', eventsUsersResponse.data);
+                console.log('UsersResponse:', usersResponse.data);
+
+                // Create a map for Events_Users
+                const eventsUsersMap = eventsUsersResponse.data.reduce((map, item) => {
+                    if (!map[item.EventID]) {
+                        map[item.EventID] = [];
+                    }
+                    map[item.EventID].push(item.UserID);
+                    return map;
+                }, {});
+
+                console.log('Events Users Map:', eventsUsersMap);
+                console.log('Current Event ID:', EventID);
+                console.log('User IDs for current event:', eventsUsersMap[EventID]);
+
+
+                // Get the list of users for the selected event
+                const updatedUsers = usersResponse.data.filter(user =>
+                    (eventsUsersMap[EventID] || []).includes(user.ID)
+                );
+
+                console.log('Filtered Users:', updatedUsers);
+
+                setUsers(updatedUsers);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [EventID]); // Re-fetch when EventID changes
+
+
+    const exportToExcel = () => {  
+        const worksheet = XLSX.utils.json_to_sheet(users);  
+        const workbook = XLSX.utils.book_new();  
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Katılımcılar");  
+        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });  
+        const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });  
+        saveAs(blob, 'katilimci-listesi.xlsx');  
+    };  
+    const handleLoGoClick =()=>{
+        navigate("/");
+    }
+
+
+    return (  
+        <div className="container">  
+            <header className="header">  
+                <img src="./logo-esbas.png" onClick={handleLoGoClick} alt="ESBAŞ Logo" className="logo" />  
+            </header>  
+            <div className="participant-list">  
+                <div className="toolbar">  
+                    <button className="excel-button" onClick={exportToExcel}>  
+                        +Excel'e Aktar  
+                    </button>  
+                </div>  
+                <h2> Katılımcı Listesi </h2> 
+                {loading ? (
+                    <p>Loading...</p>
+                ):(
+                    <div className="participants">  
+                        <table>  
+                            <thead>  
+                                <tr>  
+                                    <th> # </th>  
+                                    <th> Ad Soyad </th>  
+                                    <th> ID </th>  
+                                    <th> Departman </th>  
+                                    <th> Konum </th>  
+                                    <th> Cinsiyet </th>
+                                    <th> İşlemler</th>  
+                                </tr>  
+                            </thead>  
+                            <tbody>  
+                                {users.map((user, index) => (  
+                                    <tr key={user.ID}>  
+                                        <td> {index + 1} </td>  
+                                        <td> {user.FullName} </td>  
+                                        <td> {user.UserID} </td>  
+                                        <td> {user.Department} </td>  
+                                        <td> {user.IsOfficeEmployee} </td>  
+                                        <td> {user.Gender} </td> 
+                                        <td> 
+                                            <button className="update-button">
+                                            <FaEdit />
+                                            </button>
+                                            <button className= "delete-button">
+                                            <FaTrashAlt />
+                                            </button>
+                                        </td> 
+                                    </tr>  
+                                ))}  
+                            </tbody>  
+                        </table>  
+                    </div>  
+                   )}
+                <Link to={`/add-new-participant/${EventID}`} className="add-button">  
+                    Yeni Katılımcı  
+                </Link> 
+                
+            </div>  
+        </div>  
+    );  
+};  
+
+export default ParticipantList;  
