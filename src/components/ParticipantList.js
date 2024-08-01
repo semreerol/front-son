230a7e867cec61eaed5b520a0dc7be8aadd07611
Selman Emre Erol <106231298+semreerol@users.noreply.github.com>
2024-08-01 16:08:14 +0300

    Raporlama için seçenekler oluşturuldu

--- src/components/ParticipantList.js
+++ src/components/ParticipantList.js
@@ -1,18 +1,20 @@
 //participantlist.js
-import React, { useState, useEffect } from "react";  
-import { Link, useParams, useNavigate } from "react-router-dom"; 
-import { FaTrashAlt, FaEdit } from "react-icons/fa"; 
-import "./ParticipantList.css";  
-import * as XLSX from 'xlsx';  
-import { saveAs } from 'file-saver';  
-import axios from 'axios';  
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { FaTrashAlt, FaEdit } from "react-icons/fa";
+import "./ParticipantList.css";
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+import axios from 'axios';
+import { Cascader } from 'antd';
 
-const ParticipantList = () => {  
+
+const ParticipantList = () => {
     const { EventID } = useParams(); // Correctly destructuring to get EventID  
     const navigate = useNavigate();
-    const [users, setUsers] = useState([]); 
-    const [loading, setLoading] = useState(true); 
-    
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -58,28 +60,44 @@ const ParticipantList = () => {
     }, [EventID]); // Re-fetch when EventID changes
 
 
-    const exportToExcel = () => {  
-        const worksheet = XLSX.utils.json_to_sheet(users);  
-        const workbook = XLSX.utils.book_new();  
-        XLSX.utils.book_append_sheet(workbook, worksheet, "Katılımcılar");  
-        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });  
-        const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });  
-        saveAs(blob, 'katilimci-listesi.xlsx');  
-    };  
-    const handleLoGoClick =()=>{
+    const exportToExcel = () => {
+        const worksheet = XLSX.utils.json_to_sheet(users);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Katılımcılar");
+        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+        const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+        saveAs(blob, 'katilimci-listesi.xlsx');
+    };
+    const handleLoGoClick = () => {
         navigate("/");
     }
 
+    const options = [
+        {
+            label: 'Light',
+            value: 'light',
+        },
 
-    return (  
-        <div className="container">  
-            <header className="header">  
-                <img src={`${process.env.PUBLIC_URL}/logo-esbas.png`} onClick={handleLoGoClick} alt="ESBAŞ Logo" className="logo" />  
-            </header>  
-            <div className="participant-list">  
+        {
+            label: 'Bamboo',
+            value: 'bamboo',
+
+        },
+    ];
+    const onChange = (value) => {
+        console.log(value);
+    };
+
+
+    return (
+        <div className="container">
+            <header className="header">
+                <img src={`${process.env.PUBLIC_URL}/logo-esbas.png`} onClick={handleLoGoClick} alt="ESBAŞ Logo" className="logo" />
+            </header>
+            <div className="participant-list">
 
                 <div className="toolbar">
-                {/* <select
+                    {/* <select
             
             >
             <option value="">Seçiniz</option>
@@ -91,55 +109,96 @@ const ParticipantList = () => {
             <option value="">Seçiniz</option>
 
             </select> */}
-                    <button className="excel-button" onClick={exportToExcel}>  
-                        +Excel'e Aktar  
-                    </button>  
-                </div>  
-                <h2> Katılımcı Listesi </h2> 
+                    <Cascader
+                        className="Cascader"
+                       
+                        options={options}
+                        onChange={onChange}
+                        multiple
+                        maxTagCount="responsive"
+                    />
+                    <button className="excel-button" onClick={exportToExcel}>
+                        +Excel'e Aktar
+                    </button>
+                </div>
+                <h2> Katılımcı Listesi </h2>
                 {loading ? (
                     <p>Loading...</p>
-                ):(
-                    <div className="participants">  
-                        <table>  
-                            <thead>  
-                                <tr>  
-                                    <th> # </th>  
-                                    <th> Ad Soyad </th>  
-                                    <th> ID </th>  
-                                    <th> Departman </th>  
-                                    <th> Konum </th>  
+                ) : (
+                    <div className="participants">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th> # </th>
+                                    <th> Ad Soyad </th>
+                                    <th> ID </th>
+                                    <th> Departman </th>
+                                    <th> Konum </th>
                                     <th> Cinsiyet </th>
-                                    <th> İşlemler</th>  
-                                </tr>  
-                            </thead>  
-                            <tbody>  
-                                {users.map((user, index) => (  
-                                    <tr key={user.ID}>  
-                                        <td> {index + 1} </td>  
-                                        <td> {user.fullName} </td>  
-                                        <td> {user.userID} </td>  
-                                        <td> {user.department} </td>  
-                                        <td> {user.isOfficeEmployee} </td>  
-                                        <td> {user.gender} </td> 
-                                        <td> 
-                                            
-                                            <button className= "delete-button">
-                                            <FaTrashAlt />
-                                            </button>
-                                        </td> 
-                                    </tr>  
-                                ))}  
-                            </tbody>  
-                        </table>  
-                    </div>  
-                   )}
-                <Link to={`/add-new-participant/${EventID}`} className="add-button">  
-                    Yeni Katılımcı  
-                </Link> 
-                
-            </div>  
-        </div>  
-    );  
-};  
+                                    <th> İşlemler</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.map((user, index) => (
+                                    <tr key={user.ID}>
+                                        <td> {index + 1} </td>
+                                        <td> {user.fullName} </td>
+                                        <td> {user.userID} </td>
+                                        <td> {user.department} </td>
+                                        <td> {user.isOfficeEmployee} </td>
+                                        <td> {user.gender} </td>
+                                        <td>
 
-export default ParticipantList;  
+                                            <button className="delete-button">
+                                                <FaTrashAlt />
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+                <Link to={`/add-new-participant/${EventID}`} className="add-button">
+                    Yeni Katılımcı
+                </Link>
+
+            </div>
+        </div>
+    );
+};
+
+export default ParticipantList;
+
+/*
+import React from 'react';
+import { Cascader } from 'antd';
+const options = [
+  {
+    label: 'Light',
+    value: 'light',
+    
+    })),
+  },
+  {
+    label: 'Bamboo',
+    value: 'bamboo',
+    
+  },
+];
+const onChange = (value) => {
+  console.log(value);
+};
+const App = () => (
+  <Cascader
+    style={{
+      width: '100%',
+    }}
+    options={options}
+    onChange={onChange}
+    multiple
+    maxTagCount="responsive"
+  />
+);
+export default App;
+*/
